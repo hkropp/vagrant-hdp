a3c9cdae4f25730d57fd1a49a177488daf15196c
hkropp <hkropp@hortonworks.com>
2014-10-28 13:18:23 +0100

    set ambari stack repolist to hdp22-n3-centos6-puppet

---
+++ virtualbox/hdp22-n3-centos6-puppet/manifest/two.pp
@@ -0,0 +1,17 @@
+# VM-Configuration of an ambari agent that is monitored by the ambari server.
+
+# Turn off interfering services
+include interfering_services
+
+# Install and enable ntp
+include ntp
+
+class { 'ambari_agent':
+  serverhostname => $ambarihostname,
+  ownhostname    => $ownhostname,
+  repo => $hdp_ambari_repo
+}
+
+# Establish ordering
+Class['interfering_services'] -> Class['ntp'] -> Class['ambari_agent']
+
