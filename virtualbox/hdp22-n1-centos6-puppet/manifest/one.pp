34e36203bf026467bc5c286657cfd68d59f7436d
hkropp <hkropp@hortonworks.com>
2014-11-02 20:56:23 +0100

    added vagrant repo cache and general overhaul

--- virtualbox/hdp22-n1-centos6-puppet/manifest/one.pp
+++ virtualbox/hdp22-n1-centos6-puppet/manifest/one.pp
@@ -18,18 +18,19 @@ class { 'ambari_agent':
   repo => $hdp_ambari_repo
 }
 
-class ambari_repolist {
-    augeas { 'repolist.xml':
-        lens => "Xml.lns",
-        incl => '/var/lib/ambari-server/resources/stacks/HDP/2.2/repos/repoinfo.xml',
-        changes => [
-            "set reposinfo/os[#attribute/type='redhat6']/repo[reponame/#text='HDP']/baseurl/#text http://public-repo-1.hortonworks.com/HDP-LABS/Projects/Champlain-Preview/2.2.0.0-5/centos6",
-            "set reposinfo/os[#attribute/type='redhat6']/repo[reponame/#text='HDP-UTILS']/baseurl/#text http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.19/repos/centos6"
-
-        ]
-    }
-}
-include ambari_repolist
+#class ambari_repolist {
+#    augeas { 'repolist.xml':
+#        lens => "Xml.lns",
+#        incl => '/var/lib/ambari-server/resources/stacks/HDP/2.2/repos/repoinfo.xml',
+#        changes => [
+#            "set reposinfo/os[#attribute/type='redhat6']/repo[repoid/#text='HDP-2.2']/baseurl/#text http://public-repo-1.hortonworks.com/HDP-LABS/Projects/Champlain-Preview/2.2.0.0-706/centos6",
+#            "set reposinfo/os[#attribute/type='redhat6']/repo[repoid/#text='HDP-UTILS-1.1.0.20']/baseurl/#text http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.19/repos/centos6"
+#
+#       ]
+#    }
+#}
+#include ambari_repolist
 
 # Establish ordering
-Class['interfering_services'] -> Class['ntp'] -> Class['ambari_server'] -> Class['ambari_agent'] -> Class['ambari_repolist']
+Class['interfering_services'] -> Class['ntp'] -> Class['ambari_server'] -> Class['ambari_agent']
+#-> Class['ambari_repolist']
--- virtualbox/hdp22-n3-centos6-puppet/manifest/one.pp
+++ virtualbox/hdp22-n3-centos6-puppet/manifest/one.pp
@@ -1,35 +1,19 @@
-# VM-Configuration of the ambari server. It functions as the name node and resource manager.
-
-# Turn off interfering services
 include interfering_services
-
-# Install and enable ntp
 include ntp
 
-# Install and enable ambari server
+class {'etchosts':
+    ownhostname => $ownhostname
+}
+
 class { 'ambari_server':
   repo => $hdp_ambari_repo
 }
 
-# Install and enable ambari agent
 class { 'ambari_agent':
   ownhostname    => $ownhostname,
   serverhostname => $ambarihostname,
   repo => $hdp_ambari_repo
 }
 
-class ambari_repolist {
-    augeas { 'repolist.xml':
-        lens => "Xml.lns",
-        incl => '/var/lib/ambari-server/resources/stacks/HDP/2.2/repos/repoinfo.xml',
-        changes => [
-            "set reposinfo/os[#attribute/type='redhat6']/repo[reponame/#text='HDP']/baseurl/#text http://public-repo-1.hortonworks.com/HDP-LABS/Projects/Champlain-Preview/2.2.0.0-5/centos6",
-            "set reposinfo/os[#attribute/type='redhat6']/repo[reponame/#text='HDP-UTILS']/baseurl/#text http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.19/repos/centos6"
-
-        ]
-    }
-}
-include ambari_repolist
-
 # Establish ordering
-Class['interfering_services'] -> Class['ntp'] -> Class['ambari_server'] -> Class['ambari_agent'] -> Class['ambari_repolist']
+Class['interfering_services'] -> Class['ntp'] -> Class['etchosts'] -> Class['ambari_server'] -> Class['ambari_agent']
