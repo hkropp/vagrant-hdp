f84d293c746c35e8a11cdc20d6eb9e5fcff77dc2
hkropp <hkropp@hortonworks.com>
2014-11-02 20:46:33 +0100

    possibilty to override repo URLs for HDP 2.2

--- puppet/modules/ambari_server/manifests/init.pp
+++ puppet/modules/ambari_server/manifests/init.pp
@@ -14,6 +14,17 @@ class ambari_server ($repo) {
   package { 'ambari-server':
     ensure  => present,
     require => Exec[get-ambari-server-repo]
+  } -> 
+  
+  augeas { 'repolist.xml':
+    lens => "Xml.lns",
+    incl => '/var/lib/ambari-server/resources/stacks/HDP/2.2/repos/repoinfo.xml',
+    onlyif => "match dir[. = '/var/lib/ambari-server/resources/stacks/HDP/2.2/repos'] size != 0",
+    changes => [
+      "set reposinfo/latest/#text http://henning.kropponline.de/files/hdpqe_urlinfo.json",
+      "set reposinfo/os[#attribute/type='redhat6']/repo[repoid/#text='HDP-2.2']/baseurl/#text http://public-repo-1.hortonworks.com/HDP-LABS/Projects/Champlain-Preview/2.2.0.0-706/centos6",
+      "set reposinfo/os[#attribute/type='redhat6']/repo[repoid/#text='HDP-UTILS-1.1.0.20']/baseurl/#text http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.19/repos/centos6"
+    ]
   }
 
   exec { 'ambari-setup':
