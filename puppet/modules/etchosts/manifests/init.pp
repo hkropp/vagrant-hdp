0f438f9c021a46a0131e509774dd14882fd18a47
hkropp <hkropp@hortonworks.com>
2014-11-02 20:51:00 +0100

    vagrant hosts plugin used, here just for minor networking settings

--- puppet/modules/etchosts/manifests/init.pp
+++ puppet/modules/etchosts/manifests/init.pp
@@ -1,29 +1,6 @@
 # Ensure that the machines in the cluster can find each other without DNS
 class etchosts ($ownhostname) {
-  host { 'host_one':
-    name  => 'one.node',
-    alias => ['one', 'one.node'],
-    ip    => '192.168.0.100'
-  }
-
-  host { 'host_two':
-    name  => 'two.node',
-    alias => ['two', 'two.node'],
-    ip    => '192.168.0.102'
-  }
-
-  host { 'host_three':
-    name => 'three.node',
-    alias => ['three', 'three.node'],
-    ip => '192.168.0.103'
-  }
-
-  host { 'host_four':
-    name => 'four.node',
-    alias => ['four', 'four.node'],
-    ip => '192.168.0.104'
-  }
-
+  
   file { 'agent_hostname':
     path    => "/etc/hostname",
     ensure  => present,
