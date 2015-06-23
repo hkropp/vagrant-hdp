8ec1b29aa534d107a60a33432b42c1a0dc5b5263
hkropp <hkropp@hortonworks.com>
2015-06-23 14:04:19 +0200

    support default virtualbox

--- conf/hdp.rb
+++ conf/hdp.rb
@@ -4,7 +4,7 @@
     :hdp_stack => "2.2",
     :hdp_update => "2.2.4.2",
     :hdp_util => "1.1.0.20",
-    :vagrant_provider => "azure", # "virtualbox",
+    :vagrant_provider => "virtualbox", # "azure",
     :azure_mgmt_certificate => "/Users/hkropp/.ssh/xxxxxxx.pem",
     :azure_mgmt_endpoint => "https://management.core.windows.net",
     :azure_subscription_id => "xxxxx-xxxx-xxxxx-xxxxx-xxxxxxxxx",
@@ -17,5 +17,5 @@
     :azure_ssh_port => "22",
     :azure_ssh_private_key_file => "/Users/hkropp/.ssh/xxxx.key",
     :azure_ssh_certificate_file => "/Users/hkropp/.ssh/xxxx.pem",
-    :ambari_node => "n01-hdp-vagrant-demo.cloudapp.net", #"one.hdp",
+    :ambari_node => "one.hdp", "n01-hdp-vagrant-demo.cloudapp.net",
 }
