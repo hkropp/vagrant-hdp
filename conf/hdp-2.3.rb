061740f91c16ab9625c56f0f99b3f557721a87e4
hkropp <hkropp@hortonworks.com>
2015-06-23 13:33:04 +0200

    ruby script for hdp setup

---
+++ conf/hdp-2.3.rb
@@ -0,0 +1,9 @@
+{
+    :hdp_ambari => "2.1", # 2.1.1.0
+    :hdp_os => "centos6",
+    :hdp_stack => "2.3",
+    :hdp_update => "2.3.0.0",
+    :hdp_util => "1.1.0.20",
+    :vagrant_provider => "virtualbox",
+    :ambari_node => "one.hdp"
+}
--- conf/nodes.rb
+++ conf/nodes.rb
@@ -1,14 +1,12 @@
 [
     {
-        :name => "one.hdp", 
-        :cpu => 2, 
-        :mem => 8192, 
+        :name => "n01", 
+        :cpu => 8, 
+        :mem => 9216, 
         :ip => "192.168.33.100", 
         :manifest_file => "one.pp",
-        :tcp_endpoints => "8080,8443,10000,10001",                       # comma-seperated list of ports to forward
-        :azure_vm_image => "HDP-CentOS-66-v2",
-        :azure_vm_size => "A6",                         # Allowed values are 'ExtraSmall,Small,Medium,Large,ExtraLarge,A6,A7' http://msdn.microsoft.com/en-us/library/azure/dn197896.aspx
-        :azure_service_name => "hwx-hdp-cluster",
-        :azure_ssh_port => 22
+        :tcp_endpoints => "8080,8443,10000,10001,4040,4041,9083,8020,50070,50075,6080,8881,8882,8883,8884,8885,8886,61616,6667,2181,8081,8082,8083,8084", 
+        :azure_vm_image => 'HDP-CentOS-66-v3',
+        :azure_vm_size => "A6" # Allowed values are 'ExtraSmall,Small,Medium,Large,ExtraLarge,A6,A7' http://msdn.microsoft.com/en-us/library/azure/dn197896.aspx   
     }
 ]
