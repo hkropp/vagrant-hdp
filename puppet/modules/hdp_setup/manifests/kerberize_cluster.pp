7c332cf39dac0063b27c1891c34b0943ef378237
hkropp <hkropp@hortonworks.com>
2016-03-20 20:57:12 +0100

    added adjusted krb files

--- puppet/modules/hdp_setup/manifests/kerberize_cluster.pp
+++ puppet/modules/hdp_setup/manifests/kerberize_cluster.pp
@@ -1,11 +1,11 @@
-class hdp_setup::blueprint_install($ambarihostname='one.hdp',    ) {
+class hdp_setup::blueprint_install($ambarihostname='one.hdp', $blueprint_name='n1-hdp-basic', $krb5_kdc='one.hdp', $krb5_realm='MYCORP.NET', ) {
     include hdp_setup
     
     Exec {
         path => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/"]
     }
     
-    exec { "/vagrant/bin/kerberize_cluster.py admin admin ${ambarihostname}": 
+    exec { "/vagrant/bin/kerberize_cluster.py admin admin ${ambarihostname} 8080 ${krb5_kdc} ${krb5_realm} hdp/admin@MYCORP.NET hadoop": 
         timeout => 3600,
     }
 }   
